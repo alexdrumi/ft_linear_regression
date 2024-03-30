331616b9bcd91cb7cacdbf9286f37c0582772d4c
bmajor <mbencus89@gmail.com>
2024-03-30 19:52:46 +0100

    straight line drawed

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -1,38 +1,99 @@
 #!/opt/homebrew/bin/python3
-import math
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
 
-def create_graph_for_three(df):
-    data_np_row_three = df['km'].head(3).values
-    data_np_col_three = df['price'].head(3).values
-    
+'''
+#y = mx + b    -> in algebra
+#y = b0 + b1x  -> in statistics
+#b and b0 is the same
+#m and b1 is the same
 
-    plt.plot(data_np_row_three, data_np_col_three, label='price estimation')
-    plt.show()
+test values:
+
+n = 7
+x_sum = 28
+y_sum = 61.8
+xy_sum = 314.8
+x_squared_sum = 140
+
+expected m = 2.4142857142857146
+'''
+
+
+def plot_straight_line(x_values, y_values):
+	plt.plot(x_values, y_values, color='red', label='linear regression')
+	plt.xlabel('km')
+	plt.ylabel('price')
+	plt.title('Linear regression')
+	plt.legend()
+	plt.grid(True)
+	plt.show()
+
+
+def calculate_slope(n, x_sum, y_sum, xy_sum, x_squared_sum):
+	m_nominator = (n * xy_sum) - (x_sum * y_sum)
+	m_denominator = (n * x_squared_sum) - (x_sum ** 2)
+	m = m_nominator / m_denominator
+
+	return m
+
+
+def calculate_b_intercept(n, x_sum, y_sum, m):
+	b_nominator = y_sum - (m * x_sum)
+	b_denominator = n
+	b = b_nominator / b_denominator
+
+	return b
+
+
+def calculate_least_squares_values(df):
+	n = df.shape[0]
+	xy = df['km'] * df['price']
+	x_squared = df['km'] * df['km']
+
+	x_sum = df['km'].sum()
+	y_sum = df['price'].sum()
+	xy_sum = xy.sum()
+	x_squared_sum = x_squared.sum()
+
+	return n, x_sum, y_sum, xy_sum, x_squared_sum
+
+def least_squares(df):
+	n, x_sum, y_sum, xy_sum, x_squared_sum = calculate_least_squares_values(df)
+	
+	#we would like to find the slope for m
+	m = calculate_slope(n, x_sum, y_sum, xy_sum, x_squared_sum)
+	
+	#we would like to find b (y intercept) based on knowing m
+	b = calculate_b_intercept(n, x_sum, y_sum, m)
+
+	x_values = df['km']
+	y_values = m * x_values + b
+
+	plot_straight_line(x_values, y_values)
+
+
+
+def create_graph_for_three(df):
+	data_np_row_three = df['km'].head(3).values
+	data_np_col_three = df['price'].head(3).values
+	
+
+	plt.plot(data_np_row_three, data_np_col_three, label='price estimation', marker='o', linestyle='')
+	plt.grid(True)
+	plt.show()
 
 
 if __name__ == "__main__":
 
-    #maybe include guard in case failure
-    df = pd.read_csv('data.csv')
-    row, col = df.shape
+	#maybe include guard in case failure
+	df = pd.read_csv('data.csv')
+	row, col = df.shape
 
-    data_np_row = df['km'].values
-    data_np_col = df['price'].values
+	data_np_row = df['km'].values
+	data_np_col = df['price'].values
 
-    create_graph_for_three(df)
-    # plt.plot(data_np_row, data_np_col, label='price estimation')
-    # plt.show()
-
-
-    # print(df)
-
-    # df.T.plot()
-    # print(f' after transpose: {df}')
-
-    # df.plot()
-    # plt.show()
-    # print(df.iloc[0:row])
+	# create_graph_for_three(df)
+	least_squares(df)
