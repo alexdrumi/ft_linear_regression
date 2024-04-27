29bcc389f0bded630a1f5ec345ad10d340cdc4b1
Major Bence <bmajor@Majors-Mac-mini.local>
2024-04-27 19:46:23 +0200

    separating and making it a proper program with classes

--- class_ft_linear_regression.py
+++ class_ft_linear_regression.py
@@ -1,28 +1,57 @@
-#!/opt/homebrew/bin/python3
+#!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import sys
+import signal
+
+
 
 '''
 #!/opt/homebrew/bin/python3
 #!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
 '''
 
-
-
 class LinearRegression:
 
 	def __init__(self, df):
 		self.df = df
+
+		# mileage_normalized = min_max_normalize(df['km'].values)
+		# price_normalized = min_max_normalize(df['price'].values)
+
+		# normalized_df = pd.DataFrame({
+		# 	'km': mileage_normalized,
+		# 	'price': price_normalized
+		# })
+
+		#intercept and slope
 		self.theta0 = 0
 		self.theta1 = 0
+		self.theta0_prev = 0
+		self.theta1_prev = 0
+
+		#customized parameters based on input flags later
+		self.learning_rate = 0.01
+		self.convergence_threshold = 1e-6
+		self.iterations = 0
+		self.max_iterations = 6000
+		self.initial_treshold = 0.0001 # decay the treshold over time with 1%
+
+		#derivatives
+		self.derivative_intercept = 0
+		self.derivative_slope = 0
+
+		#data
+		self.mileage = self.min_max_normalize(df['km'].values)
+		self.price = self.min_max_normalize(df['price'].values)
+
 
 
 
 	def calculate_least_squares_values(self, df):
 		n = df.shape[0]
-		# n = 3
 		xy = df['km'] * df['price']
 		x_squared = df['km'] * df['km']
 
@@ -80,12 +109,12 @@ class LinearRegression:
 
 
 
-	def derivative_MSE(self, mileage, price, intercept, slope):
-		n = len(price)
-		y = price
-		x = mileage
-		b = intercept
-		c = slope
+	def derivative_MSE(self):
+		y = self.price
+		x = self.mileage
+		b = self.theta0
+		c = self.theta1
+		n = len(self.price)
 		h = y - (b + c * x)
 
 		dh_db = -1
@@ -97,34 +126,38 @@ class LinearRegression:
 		return (dMSE_db, dMSE_dc)
 	
 
+	def min_max_normalize(self, array):
+		x_min = np.min(array)
+		x_max = np.max(array)
+
+		array_normalized = (array - x_min)/(x_max - x_min)
+		return array_normalized
+
 
 	def gradient_descent(self, df):
-		mileage = np.array(df['km'])
-		price = np.array(df['price'])
-		theta0, theta1 = 0, 0
 		learning_rate = 0.01
 		convergence_threshold = 1e-6
 		max_iterations = 6000
 
-		derivative_intercept, derivative_slope = self.derivative_MSE(self, mileage, price, theta0, theta1)
-		theta0_prev, theta1_prev = theta0, theta1
+		self.derivative_intercept, self.derivative_slope = self.derivative_MSE()
+		self.theta0_prev, self.theta1_prev = self.theta0, self.theta1
 
 		mse_history = []
 		mse_history.append(0)
 		initial_treshold = 0.0001
 		while (max_iterations > 0):
-			derivative_intercept, derivative_slope = self.derivative_MSE(mileage, price, theta0, theta1)
+			self.derivative_intercept, self.derivative_slope = self.derivative_MSE()
 
-			step_size_intercept = learning_rate * derivative_intercept
-			step_size_slope = learning_rate * derivative_slope 
+			step_size_intercept = self.learning_rate * self.derivative_intercept
+			step_size_slope = self.learning_rate * self.derivative_slope 
 
-			theta0 = theta0_prev - step_size_intercept
-			theta1 = theta1_prev - step_size_slope
+			self.theta0 = self.theta0_prev - step_size_intercept
+			self.theta1 = self.theta1_prev - step_size_slope
 
-			mse_current = np.mean((price - (theta0 + theta1 * mileage))**2)
+			mse_current = np.mean((self.price - (self.theta0 + self.theta1 * self.mileage))**2)
 			mse_history.append(mse_current)
 
-			if (max_iterations != 6000 and abs(mse_history[-2] - mse_history[-1]) < initial_treshold):
+			if (self.max_iterations != 6000 and abs(mse_history[-2] - mse_history[-1]) < self.initial_treshold):
 				print('breaking because MSE didnt change much')
 				break 
 			initial_treshold *= 0.99
@@ -135,6 +168,8 @@ class LinearRegression:
 			# 	break 
 			
 			# print(f'{theta0} compared to {least_squares_intercept} and {theta1} compared to {least_squares_slope}\n')
+			print(f'{theta0} compared to and {theta1} compared to \n')
+
 			theta0_prev = theta0
 			theta1_prev = theta1
 
@@ -142,4 +177,32 @@ class LinearRegression:
 		
 		return theta0, theta1
 
+
+
+def signal_handler():
+	sys.exit(0)
+
+
+
+if __name__ == '__main__':
+	signal.signal(signal.SIGINT, signal_handler)
+
+	df = pd.read_csv('data.csv')
+	row, col = df.shape
+
+
+
+	linear_regression_instance = LinearRegression(df)
+
+	result = linear_regression_instance.gradient_descent(df)
+	print(result)
+	# mileage_normalized = min_max_normalize(df['km'].values)
+	# price_normalized = min_max_normalize(df['price'].values)
+
+	# normalized_df = pd.DataFrame({
+	# 	'km': mileage_normalized,
+	# 	'price': price_normalized
+	# })
+
+	# theta1, theta2 = self.gradient_descent(normalized_df)
 
