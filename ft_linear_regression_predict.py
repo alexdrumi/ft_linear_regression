415b4bca0afaa7ccdb7a9585647fc9f0c3ec6bca
Major Bence <bmajor@Majors-Mac-mini.local>
2024-04-28 19:12:58 +0200

    separating two programs on the way

---
+++ ft_linear_regression_predict.py
@@ -0,0 +1,56 @@
+#!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
+
+#read thetas from file
+#predict a given value from the terminal
+
+import numpy as np
+import pandas as pd
+'''
+!/opt/homebrew/bin/python3
+!/Library/Frameworks/Python.framework/Versions/3.10/bin/python3
+'''
+
+'''
+Normalizes a value using min-max normalization
+'''
+def min_max_normalize(value, min_val, max_val):
+	return (value - min_val) / (max_val - min_val)
+
+
+'''
+Loads the trained theta values and uses them to predict price based on a given mileage.
+'''
+def load_thetas():
+	with open('thetas.txt', 'r') as file:
+		theta0 = float(file.readline().strip())
+		theta1 = float(file.readline().strip())
+	return theta0, theta1
+
+'''
+Predicts the price based on mileage using the trained thetas and min/max values for normalization
+'''
+def predict_price(mileage, theta0, theta1, min_km, max_km):
+	normalized_mileage = min_max_normalize(mileage, min_km, max_km)
+	return theta0 + theta1 * normalized_mileage
+
+
+
+if __name__ == '__main__':
+	# Load thetas
+	# read mileage arguiments from the command line
+
+	#error check?
+	mileage = float(input("Enter the mileage of the car: "))
+	df = pd.read_csv('data.csv')
+
+	min_km, max_km = np.min(df['km'].values), np.max(df['km'].values)
+
+	theta0, theta1 = load_thetas()
+
+	# These min and max values should be the same as used during training normalization
+	# min_km = 5000  # Example min km, replace with actual min value from tra
+	prediction = predict_price(mileage, theta0, theta1, min_km, max_km)
+	print(prediction)
+
+
+
